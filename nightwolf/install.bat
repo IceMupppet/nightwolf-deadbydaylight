dda9e7fcdbf6915cfbbb84fc79963ada2d48ffa6
IceMupppet <icemuppet+github@gmail.com>
2020-12-19 19:50:21 -0700

    Delete install.bat

--- nightwolf/install.bat
+++
@@ -1,8 +0,0 @@
-ECHO "Dead by daylight : Nightwolf Edition"
-
-ECHO "THIS IS JUST A TEST! - REMOVE BEFORE RUNNING!"
-
-:: PERKS
-MOVE /Y content/iconPerks_quickAndQuiet.png, iconPerks_quickAndQuiet.png ../../DeadByDaylight/Content/UI/Icons/Perks/
-
-
